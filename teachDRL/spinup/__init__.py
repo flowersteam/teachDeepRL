dd1eec838333ba98aaf1a8d94d25279c3f2ed40d
rPortelas <remy.portelas@gmail.com>
2019-10-18 13:29:54 +0200

    fix spinup imports v2

--- teachDRL/spinup/__init__.py
+++ teachDRL/spinup/__init__.py
@@ -1,9 +1,9 @@
 # Algorithms
-from spinup.algos.ddpg.ddpg import ddpg
-from spinup.algos.ppo.ppo import ppo
-from spinup.algos.sac.sac import sac
-from spinup.algos.td3.td3 import td3
-from spinup.algos.trpo.trpo import trpo
-from spinup.algos.vpg.vpg import vpg
+#from spinup.algos.ddpg.ddpg import ddpg
+#from spinup.algos.ppo.ppo import ppo
+from teachDRL.spinup.algos.sac.sac import sac
+#from spinup.algos.td3.td3 import td3
+#from spinup.algos.trpo.trpo import trpo
+#from spinup.algos.vpg.vpg import vpg
 # Loggers
-from spinup.utils.logx import Logger, EpochLogger
+from teachDRL.spinup.utils.logx import Logger, EpochLogger
